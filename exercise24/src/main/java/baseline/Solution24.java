3452f461523a297b92296dd801ac0efd293248a9
richard lopez <richardlopez813>
2021-10-02 23:43:58 -0400

    first commit

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,21 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Richard Lopez
+ */
+public class Solution24 {
+    public static void main(String[] args) {
+        //prompt to enter strings
+        //scan for first string
+        //scan for second string
+        //call isAnagram function
+        //print result
+    }
+
+    public boolean isAnagram(String string1, String string2){
+        //reverse string1
+        //loop through indices until elements don't match or string length reached
+        //if same return true
+        //else return false
+    }
+}
